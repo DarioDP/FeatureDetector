9681a413c9695e1f372ac60be579e37fbf474aaa
Ingo Buerk <admin@airblader.de>
2012-12-05 15:35:49 +0100

    Added remaining features

--- FeatureDetector.js
+++ FeatureDetector.js
@@ -44,39 +44,61 @@ cordova.define('cordova/plugin/FeatureDetector', function (require, exports, mod
     // https://developer.android.com/reference/android/content/pm/PackageManager.html
     FeatureDetector.prototype.FEATURE = {
         // CAMERA
-        FEATURE_CAMERA                        : 'android.hardware.camera',
-        FEATURE_CAMERA_ANY                    : 'android.hardware.camera.any',
-        FEATURE_CAMERA_AUTOFOCUS              : 'android.hardware.camera.autofocus',
-        FEATURE_CAMERA_FLASH                  : 'android.hardware.camera.flash',
-        FEATURE_CAMERA_FRONT                  : 'android.hardware.camera.front',
+        FEATURE_CAMERA                          : 'android.hardware.camera',
+        FEATURE_CAMERA_ANY                      : 'android.hardware.camera.any',
+        FEATURE_CAMERA_AUTOFOCUS                : 'android.hardware.camera.autofocus',
+        FEATURE_CAMERA_FLASH                    : 'android.hardware.camera.flash',
+        FEATURE_CAMERA_FRONT                    : 'android.hardware.camera.front',
         
         // FAKETOUCH
-        FEATURE_FAKETOUCH                     : 'android.hardware.faketouch',
-        FEATURE_FAKETOUCH_MULTITOUCH_DISTINCT : 'android.hardware.faketouch.multitouch.distinct',
-        FEATURE_FAKETOUCH_MULTITOUCH_JAZZHAND : 'android.hardware.faketouch.multitouch.jazzhand',
+        FEATURE_FAKETOUCH                       : 'android.hardware.faketouch',
+        FEATURE_FAKETOUCH_MULTITOUCH_DISTINCT   : 'android.hardware.faketouch.multitouch.distinct',
+        FEATURE_FAKETOUCH_MULTITOUCH_JAZZHAND   : 'android.hardware.faketouch.multitouch.jazzhand',
         
         // LOCATION
-        FEATURE_LOCATION                      : 'android.hardware.location',
-        FEATURE_LOCATION_GPS                  : 'android.hardware.location.gps',
-        FEATURE_LOCATION_NETWORK              : 'android.hardware.location.network',
+        FEATURE_LOCATION                        : 'android.hardware.location',
+        FEATURE_LOCATION_GPS                    : 'android.hardware.location.gps',
+        FEATURE_LOCATION_NETWORK                : 'android.hardware.location.network',
         
         // SENSOR
-        FEATURE_SENSOR_ACCELEROMETER          : 'android.hardware.sensor.accelerometer',
-        FEATURE_SENSOR_BAROMETER              : 'android.hardware.sensor.barometer',
-        FEATURE_SENSOR_COMPASS                : 'android.hardware.sensor.compass',
-        FEATURE_SENSOR_GYROSCOPE              : 'android.hardware.sensor.gyroscope',
-        FEATURE_SENSOR_LIGHT                  : 'android.hardware.sensor.light',
-        FEATURE_SENSOR_PROXIMITY              : 'android.hardware.sensor.proximity',
+        FEATURE_SENSOR_ACCELEROMETER            : 'android.hardware.sensor.accelerometer',
+        FEATURE_SENSOR_BAROMETER                : 'android.hardware.sensor.barometer',
+        FEATURE_SENSOR_COMPASS                  : 'android.hardware.sensor.compass',
+        FEATURE_SENSOR_GYROSCOPE                : 'android.hardware.sensor.gyroscope',
+        FEATURE_SENSOR_LIGHT                    : 'android.hardware.sensor.light',
+        FEATURE_SENSOR_PROXIMITY                : 'android.hardware.sensor.proximity',
+    
+        // TELEPHONY
+        FEATURE_TELEPHONY                       : 'android.hardware.telephony',
+        FEATURE_TELEPHONY_CDMA                  : 'android.hardware.telephony.cdma',
+        FEATURE_TELEPHONY_GSM                   : 'android.hardware.telephony.gsm',
+        
+        // TOUCHSCREEN
+        FEATURE_TOUCHSCREEN                     : 'android.hardware.touchscreen',
+        FEATURE_TOUCHSCREEN_MULTITOUCH          : 'android.hardware.touchscreen.multitouch',
+        FEATURE_TOUCHSCREEN_MULTITOUCH_DISTINCT : 'android.hardware.multitouch.distinct',
+        FEATURE_TOUCHSCREEN_MULTITOUCH_JAZZHAND : 'android.hardware.multitouch.jazzhand',
         
         // OTHER HARDWARE
-        FEATURE_MICROPHONE                    : 'android.hardware.microphone',
-        FEATURE_NFC                           : 'android.hardware.nfc',
-        FEATURE_SCREEN_LANDSCAPE              : 'android.hardware.screen.landscape',
-        FEATURE_SCREEN_PORTRAIT               : 'android.hardware.screen.portrait',
+        FEATURE_MICROPHONE                      : 'android.hardware.microphone',
+        
+        FEATURE_NFC                             : 'android.hardware.nfc',
+        
+        FEATURE_SCREEN_LANDSCAPE                : 'android.hardware.screen.landscape',
+        FEATURE_SCREEN_PORTRAIT                 : 'android.hardware.screen.portrait',
+        
+        FEATURE_TELEVISION                      : 'android.hardware.type.television',
+        
+        FEATURE_USB_ACCESSORY                   : 'android.hardware.usb.accessory',
+        FEATURE_USB_HOST                        : 'android.hardware.usb.host',
+        
+        FEATURE_WIFI                            : 'android.hardware.wifi',
+        FEATURE_WIFI_DIRECT                     : 'android.hardware.wifi.direct',
         
         // SOFTWARE
-        FEATURE_LIVE_WALLPAPER                : 'android.software.live_wallpaper',
-        FEATURE_SIP                           : 'android.software.sip',
+        FEATURE_LIVE_WALLPAPER                  : 'android.software.live_wallpaper',
+        FEATURE_SIP                             : 'android.software.sip',
+        FEATURE_SIP_VOIP                        : 'android.software.sip.voip',
     };
     
     
